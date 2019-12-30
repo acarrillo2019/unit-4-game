4d0ea580cc9f5e9e71b27b355ee1ea259a981536
acarrillo2019 <alric21@aol.com>
2019-12-29 19:03:29 -0700

    Game mechanics are ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,110 @@
-
 $( document ).ready(function() {
-/*
-Here's how the app works:
 
-   * There will be four crystals displayed as buttons on the page.
-        - each crystal will have a different value. One will be worth 1, another worth 5, another worth 25, and another worth 50.
+   resetGame();
 
-   * The player will be shown a random number at the start of the game.
-        - Create random number generator and display it in its own div on page.
+   $(".jewelImages").on("click",function() {
+      // console.log($(this).attr("id"));
+      // console.log(this);
 
-   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+      // Clear previous game status on first gem click in new game
+      if (playerScore===0) {
+          $("#status").fadeOut();
+          // $("#status").html("");
+      }
 
-     * Your game will hide this amount until the player clicks a crystal. 
-        - The crystal values will randomize each game.
-     * When they do click one, update the player's score counter.
-        -Crystal clicks/worths will update on counter in its own div.
+      // Add selected gem value to playerScore
+      addScore($(this).attr("id"));
 
-   * The player wins if their total score matches the random number from the beginning of the game.
-
-   * The player loses if their score goes above the random number.
-
-   * The game restarts whenever the player wins or loses.
-
-     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-*/
-
-let jewel1; // = $("#jewel1") 
-let jewel2; // = $("#jewel2")
-let jewel3; // = $("#jewel3")
-let jewel4; //= $("#jewel4")
+      if (playerScore === randGet) {
+          wins++;
+          $("#status").html("You Win!");
+          $("#status").fadeIn();
+          resetGame();
+      }
+      else if (playerScore > randGet) {
+          losses++;
+          $("#status").html("You Lose!");
+          $("#status").fadeIn();
+          resetGame();
+      }
+});
+})
 
 var playerScore;
-var randomScore;
+var randGet;
 var wins = 0;
 var losses = 0;
 
-let assign1;
+var jewel1 = new Gem(); 
+var jewel2 = new Gem();
+var jewel3 = new Gem();
+var jewel4 = new Gem();
 
-const jewelValues = [1, 5, 25 ,50]
-
-
-// random number generator
-const numArray = () => {
-    // Function to generate random number  
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
-    randGet = (randomNumber(19, 99));
-    return randGet;
+function Gem (value) {
+   this.value = value;
 }
 
-const assignValues = () => { 
-   ser = jewelValues[Math.floor(Math.random() * jewelValues.length)];
-   //var pop1 = assign1;
-    /*  if (assign1 = 1) {
-         jewel1 = 1;
-         jewel2 = 50;
-         jewel3 = 5;
-         jewel4 = 25;
-      } else if (assign1 = 5) {
-         jewel1 = 5;
-         jewel2 = 1;
-         jewel3 = 50;
-         jewel4 = 25;
-      } else if (assign1 = 25) {
-         jewel1 = 25;
-         jewel2 = 50;
-         jewel3 = 1;
-         jewel4 = 5
-      } else if (assign1 = 50) {
-         jewel1 = 50;
-         jewel2 = 1;
-         jewel3 = 25;
-         jewel4 = 5;   
-      } */
-
-      console.log(assignValues())
+function addScore(gem) {
+   switch(gem) {
+       case "jewel1":
+           playerScore += jewel1.value;
+           break;
+       case "jewel2":
+           playerScore += jewel2.value;
+           break;
+       case "jewel3":
+           playerScore += jewel3.value;
+           break;
+       case "jewel4":
+           playerScore += jewel4.value;
+           break;
+   }
+   // Display the new score
+   $("#playerScore").html(playerScore);
 }
 
+function resetGame() {
+   var powersArr = [];
+   playerScore = 0;
+   const numArray = () => {
+      // Function to generate random number  
+      function randomNumber(min, max) {
+          return Math.floor(Math.random() * (max - min) + min);
+      }
+      randGet = (randomNumber(19, 99));
+      return randGet;
+  }
+   
+
+   jewel1.value = getRandomPower(powersArr);
+   jewel2.value = getRandomPower(powersArr);
+   jewel3.value = getRandomPower(powersArr);
+   jewel4.value = getRandomPower(powersArr);
 
 
 
-$("#thirdRowBox").html(numArray);
-$("#wins").html(wins);
-$("#losses").html(losses);
+   console.log("Red:" + jewel1.value);
+   console.log("Blue:" + jewel2.value);
+   console.log("Green:" + jewel3.value);
+   console.log("Purple:"+ jewel4.value);
 
-});
+
+   // Display values
+   $("#playerScore").html(playerScore);
+   $("#thirdRowBox").html(numArray);
+   $("#wins").html(wins);
+   $("#losses").html(losses);
+}
+ 
+function getRandomPower(pa) {
+   var power;
+
+   // Loop until a unique power value is generated
+   do {
+       power = Math.floor(Math.random() * 12)+1;
+   } while(pa.includes(power));
+
+   pa.push(power);
+
+   return power;
+}
